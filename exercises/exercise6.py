6d0bb4e860566638bde31835f3e2c963ad0f8961
garcilazojuancruz95 <garcilazo.juancruz95@gmail.com>
2024-12-02 21:17:33 -0300

    Cambios ejercicio 6

--- exercises/exercise6.py
+++ exercises/exercise6.py
@@ -21,7 +21,7 @@ Restricción: Utilizar el método pop
 lista = ["ho", "la", 81, 6, 42, "como", "estas?"]
 
 # COMPLETAR - INICIO
-
+elemento_extraido = lista.pop(3)
 # COMPLETAR - FIN
 
 assert elemento_extraido == 6
--- exercises/test-exercise.py
+++ exercises/test-exercise.py
@@ -1,11 +1,17 @@
 """
-Inicializar una lista vacía y luego agregarle 4 elementos cualquiera
-Restricción: Utilizar el método append
+Concatenar las siguientes listas
+Restricción: Utilizar el método extend
 """
 
+lista_a = [1, 2, 3]
+lista_b = ["4", "5", "6"]
+lista_c = ["siete", "ocho", "nueve"]
+
 # COMPLETAR - INICIO
-lista_01 = ["Hola", 4, "V"]
-lista_01.append("Edgerunner")
+listas_concatenadas_01 = lista_a
+listas_concatenadas_01 = lista_b
+listas_concatenadas_01.extend(lista_c)
 # COMPLETAR - FIN
 
-assert len(lista_01) == 4
+print(listas_concatenadas_01)
+assert listas_concatenadas_01 == [1, 2, 3, "4", "5", "6", "siete", "ocho", "nueve"]
